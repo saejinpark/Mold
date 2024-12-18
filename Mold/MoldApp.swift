41f017b58d876ad60706ee6979fa9113f601b6d0
saejinpark <saejinpark951210@gmail.com>
2024-12-18 14:12:47 +0900

    chore: 프로젝트 초기 설정

---
+++ Mold/MoldApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoldApp.swift
+//  Mold
+//
+//  Created by 박세진 on 12/15/24.
+//
+
+import SwiftUI
+
+@main
+struct MoldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
